eeb918a9eb452b8912c09d5e0e90a0712924884a
Orlov Stepan <stepano@dev.pkb-rio.com>
2017-12-24 21:32:19 +0300

    Added the Debug tests launch configuration (tests run not sandboxed). Working on test-03-so.

--- routes/task-output.js
+++ routes/task-output.js
@@ -8,6 +8,8 @@ var allTasks = require('./all-tasks.js')
 
 var taskRunner = require('./task-runner.js')()
 
+var sandboxed = process.env.DEBUG_TASKS? false: true
+
 router
     .get('/task-output', function(req, res, next) {
         var taskId = req.query.taskId || 'test-01-so:1'
@@ -17,21 +19,28 @@ router
             height: util.clamp(req.query.height, 1, 50, 24),
             stdin: req.query.stdin || task.stdin()
         }
-        taskRunner.run(taskId, taskParam, 500)
-            .on('done', function(output) {
-                res.send(JSON.stringify({
-                    width: taskParam.width,
-                    height: taskParam.height,
-                    data: output.join('\n')
-                }))
-            })
-            .on('error', function(output, message) {
-                res/*.status(500)*/.send(JSON.stringify({
-                    width: taskParam.width,
-                    height: taskParam.height,
-                    data: output.length > 0 ?  output.join('\n') + '\n...\n' + message :   message
-                }))
-            })
+        if (sandboxed)
+            taskRunner.run(taskId, taskParam, 500)
+                .on('done', function(output) {
+                    res.send(JSON.stringify({
+                        width: taskParam.width,
+                        height: taskParam.height,
+                        data: output.join('\n')
+                    }))
+                })
+                .on('error', function(output, message) {
+                    res/*.status(500)*/.send(JSON.stringify({
+                        width: taskParam.width,
+                        height: taskParam.height,
+                        data: output.length > 0 ?  output.join('\n') + '\n...\n' + message :   message
+                    }))
+                })
+        else
+            res.send(JSON.stringify({
+                width: taskParam.width,
+                height: taskParam.height,
+                data: task.scene().paint(taskParam.width, taskParam.height, taskParam.stdin)
+            }))
 
         /*
         // Note: This code was previously used to run tasks within the main process
@@ -41,11 +50,6 @@ router
         var w = util.clamp(req.query.width, 1, 160, 80)
         var h = util.clamp(req.query.height, 1, 50, 24)
         var stdin = req.query.stdin || task.stdin()
-        res.send(JSON.stringify({
-            width: w,
-            height: h,
-            data: task.scene().paint(w, h, stdin)
-        }))
         */
     })
 
