3f9b433d3dfebfe980d0e259be9d8d3df4090c11
Stepan Orlov <majorsteve@mail.ru>
2017-12-26 16:23:13 +0300

    Working on task tags, take 2.

--- routes/all-tasks.js
+++ routes/all-tasks.js
@@ -6,16 +6,24 @@ var tasksDir = path.join(__dirname, '..', 'tasks')
 
 var tasks = {}
 var taskSetDescriptions = {}
+var taskTags = []
 
 ;(function() {
+    var taskTagsObject = {}
     fs.readdirSync(path.join(tasksDir)).forEach(function(id) {
         var taskSet = require(path.join(tasksDir, id))
         var dirTasks = taskSet.data.items
         id = id.replace(/\.js$/, '')
         taskSetDescriptions[id] = taskSet.data.description
-        for (var i=0, n=dirTasks.length; i<n; ++i)
-            tasks[id + ':' + (i+1)] = dirTasks[i]
+        for (var i=0, n=dirTasks.length; i<n; ++i) {
+            var task = dirTasks[i]
+            _.each(task.tags(), function(tag) {
+                taskTagsObject[tag] = true
+            })
+            tasks[id + ':' + (i+1)] = task
+        }
     })
+    taskTags = _.keys(taskTagsObject).sort()
 })()
 
 function taskSetFilter(taskSet) {
@@ -57,6 +65,7 @@ function taskSetDescription(taskSet) {
 module.exports = {
     tasks: function() { return tasks },
     taskIds: taskIds,
+    taskTags: function() { return taskTags },
     allTaskIds: allTaskIds,
     taskSetFilter: taskSetFilter,
     taskIdFilter: taskIdFilter,
